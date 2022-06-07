539477fc90d9787d044a2450bff4aa4a873282c4
Arliey <xrley12@gmail.com>
2022-06-07 19:33:13 +0700

    Modul 5

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index', [
+        "title" => "Beranda"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "nama" => "Arliey Januar",
+        "email" => "xrley12@gmail.com",
+        "gambar" => "jusiwot.jpeg"
+    ]);
+});
+
+Route::get('/gallery', function () {
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
+});
+
+Route::resource('/contacts', ContactController::class);
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function () {
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
